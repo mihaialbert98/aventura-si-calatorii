84f247907cc7ebd553daa9a9caa938164227e124
Mihai Albert <mihai.albert.ioan@gmail.com>
2026-03-19 19:57:22 +0200

    feat: update image remote patterns to allow all HTTPS sources

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,7 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'plus.unsplash.com' },
-      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: '**' },
     ],
   },
 };
